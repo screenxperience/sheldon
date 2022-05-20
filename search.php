d52c6f502a20b93cb85741786c0cbd9e385804b0
screenxperience <102996339+screenxperience@users.noreply.github.com>
2022-05-20 12:34:12 +0200

    Add files via upload

--- search.php
+++ search.php
@@ -73,7 +73,7 @@ else
 								
 							$serialnr = $filter[4];
 
-							$output .= '<ul class="flex block">';
+							$i = 0;
 
 							$query = " 
 							SELECT lend_id,user_name,user_vname,lend_user_id,lend_start
@@ -105,13 +105,76 @@ else
 									$query .= " OR lend_assets LIKE '".'%"'.$row['asset_id'].'"%'."' OR lend_archived_assets LIKE '".'%"'.$row['asset_id'].'"%'."'";
 								}
 							}
-							
+
+							$result = $sql->query($query);
+
+							$amount_gs = mysqli_num_rows($result);
+
+							while($row = $result->fetch_array(MYSQLI_ASSOC))
+							{
+								if($i == 0)
+								{
+									$output .= '<div class="container">';
+									$output .= '<h1>Leihgaben suchen ( '.$amount_gs.' )</h1>';
+									$output .= '</div>';
+									$output .= '<ul class="flex block">';
+								}
+
+								if($i == 2)
+								{
+									$output .= '</ul>';
+									$output .= '<ul class="flex block">';
+
+									$i = 0;
+								}
+
+								$lend_start = date('d.m.Y',strtotime($row['lend_start']));
+
+								$output .= '<li class="col-s12 col-m6 col-l6">';
+								$output .= '<div class="text-center-medium text-center-small margin container display-container black-alpha">';
+								$output .= '<p>'.$row['lend_user_id'].'</p>';
+								$output .= '<p>'.$row['user_name'].', '.$row['user_vname'].'</p>';
+								$output .= '<p>verliehen am '.$lend_start.'</p>';
+								$output .= '<div class="container-large display-middle-right-large section-medium section-small">';
+								$output .= '<a class="btn-default border border-light-blue light-blue hover-white hover-text-blue" href="view.php?category='.$_GET['category'].'&id='.$row['lend_id'].'"><i class="fas fa-eye"></i></a>';
+								$output .= '</div>';
+								$output .= '</div>';
+								$output .= '</li>';
+
+								$i++;
+							}
+
+							$output .= '</ul>';
+						}
+						else if($_GET['category'] == $allowed_category[1])
+						{
+							$type_id = $filter[0]; 
+								
+							$vendor_id = $filter[1]; 
+								
+							$model_id = $filter[2];  
+								
+							$serialnr = $filter[4];
+
+							$query = sprintf("
+							SELECT asset_id,type_name,vendor_name,model_name,asset_serial
+							FROM asset
+							INNER JOIN type ON type_id = asset_type_id
+							INNER JOIN vendor ON vendor_id = asset_vendor_id
+							INNER JOIN model ON model_id = asset_model_id
+							WHERE asset_type_id = '%s'
+							OR asset_vendor_id = '%s'
+							OR asset_model_id = '%s'
+							OR asset_serial LIKE '%s';",
+							$sql->real_escape_string($type_id),
+							$sql->real_escape_string($vendor_id),
+							$sql->real_escape_string($model_id),
+							$sql->real_escape_string('%'.$serialnr.'%'));
+
 							$result = $sql->query($query);
 
 							while($row = $result->fetch_array(MYSQLI_ASSOC))
 							{
-								$lend_start = date('d.m.Y',strtotime($row['lend_start']));
-
 								if($i == 2)
 								{
 									$output .= '</ul>';
@@ -122,11 +185,12 @@ else
 
 								$output .= '<li class="col-s12 col-m6 col-l6">';
 								$output .= '<div class="text-center-medium text-center-small margin container display-container black-alpha">';
-								$output .= '<p>'.$row['lend_user_id'].'</p>';
-								$output .= '<p>'.$row['user_name'].', '.$row['user_vname'].'</p>';
-								$output .= '<p>verliehen am '.$lend_start.'</p>';
+								$output .= '<p>'.$row['type_name'].' / '.$row['vendor_name'].' / '.$row['model_name'].'</p>';
+								$output .= '<p>'.$row['asset_serial'].'</p>';
 								$output .= '<div class="container-large display-middle-right-large section-medium section-small">';
-								$output .= '<a class="btn-default border border-light-blue light-blue hover-white hover-text-blue" href="view.php?category='.$_GET['category'].'&id='.$row['lend_id'].'"><i class="fas fa-eye"></i></a>';
+								$output .= '<a class="btn-default border border-light-blue light-blue hover-white hover-text-blue" href="del.php?category='.$_GET['category'].'&id='.$row['asset_id'].'&returnto='.urlencode('http://'.$_SERVER['HTTP_HOST'].'/list.php?category='.$_GET['category'].'&site='.$_GET['site'].'&amount='.$_GET['amount']).'"><i class="fas fa-trash"></i></a> ';
+								$output .= '<a class="btn-default border border-light-blue light-blue hover-white hover-text-blue" href="view.php?category='.$_GET['category'].'&id='.$row['asset_id'].'&tab=general"><i class="fas fa-eye"></i></a> ';
+								$output .= '<a class="btn-default border border-light-blue light-blue hover-white hover-text-blue" href="cart.php?aktion=add&category='.$_GET['category'].'&id='.$row['asset_id'].'"><i class="fas fa-shopping-cart"></i> <i class="fas fa-plus"></i></a>';
 								$output .= '</div>';
 								$output .= '</div>';
 								$output .= '</li>';
